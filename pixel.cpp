ffaa94181c87e2d4358150525294c19a6c1089a6
DASTLSITO <dastlhelp@gmail.com>
2025-10-29 19:10:51 -0700

    feat: adding pixel class

---
+++ pixel.cpp
@@ -0,0 +1,13 @@
+#include "pixel.h"
+
+pixel::pixel() {
+    this->value = 0;
+    this->x = 0;
+    this->y = 0;
+}
+
+pixel::pixel(uint8_t value, int x, int y){
+    this->value = value;
+    this->x = x;
+    this->y = y;
+}
---
+++ pixel.h
@@ -0,0 +1,28 @@
+#ifndef PIXEL_H
+#define PIXEL_H
+
+#include "cstdint"
+
+class pixel
+{
+public:
+    // Constructors
+    pixel();
+    pixel(uint8_t, int, int);
+
+    // Destructors
+    ~pixel();
+
+    // Attributes
+    uint8_t value;
+    int x;
+    int y;
+
+    // Methods
+    pixel copy();
+
+
+
+};
+
+#endif // PIXEL_H
