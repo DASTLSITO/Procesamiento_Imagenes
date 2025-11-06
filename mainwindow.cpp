b354f7db558c2803423e096b51dd3c9ff056d2ce
DASTLSITO <dastlhelp@gmail.com>
2025-11-05 18:55:10 -0700

    feat: Adding new image methods

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 using namespace cv;
 
-Mat imageMat;
+Mat matOriginal;
 Image image;
 
 MainWindow::MainWindow(QWidget *parent)
@@ -34,14 +34,58 @@ Image* MatToImage(Mat& imageMat){
     return new Image(columns, rows, pixels);
 }
 
-Mat GetInverseImage(Mat& originalImage){
-    Mat imageCopy = originalImage.clone();
-    for(int i = 0; i < imageCopy.rows; i++){
-        for(int j = 0; j < imageCopy.cols; j++){
-            imageCopy.at<uint8_t>(i, j) = 255 - imageCopy.at<uint8_t>(i, j);
+Mat GetInverseImage(Mat& originalMat){
+    Mat matCopy = originalMat.clone();
+    for(int i = 0; i < matCopy.rows; i++){
+        for(int j = 0; j < matCopy.cols; j++){
+            matCopy.at<uint8_t>(i, j) = 255 - matCopy.at<uint8_t>(i, j);
         }
     }
-    return imageCopy;
+    return matCopy;
+}
+
+Mat GetBinaryImage(Mat& originalMat, uint8_t threshold = 125){
+    Mat matCopy = originalMat.clone();
+    for(int i = 0; i < matCopy.rows; i++){
+        for(int j = 0; j < matCopy.cols; j++){
+            matCopy.at<uint8_t>(i, j) = originalMat.at<uint8_t>(i, j) < threshold ? 0 : 255;
+        }
+    }
+    return matCopy;
+}
+
+Mat GetMinimumValueImage(Mat& originalMat, Mat& thresholdMat){
+    Mat matCopy = originalMat.clone();
+    for(int i = 0; i < matCopy.rows; i++){
+        for(int j = 0; j < matCopy.cols; j++){
+            matCopy.at<uint8_t>(i, j) = originalMat.at<uint8_t>(i, j) < thresholdMat.at<uint8_t>(i, j)
+                                            ? originalMat.at<uint8_t>(i, j)
+                                            : thresholdMat.at<uint8_t>(i, j);
+        }
+    }
+    return matCopy;
+}
+
+Mat GetMirrorImage(Mat& originalMat){
+    Mat matCopy = Mat(originalMat.cols, originalMat.rows, originalMat.type());
+    for(int i = 0; i < matCopy.rows; i++){
+        for(int j = 0; j < matCopy.cols; j++){
+            matCopy.at<uint8_t>(i, j) = originalMat.at<uint8_t>(j, i);
+        }
+    }
+    return matCopy;
+}
+
+
+
+Mat GetMirror90DImage(Mat& originalMat){
+    Mat matCopy = Mat(originalMat.cols, originalMat.rows, originalMat.type());
+    for(int i = 0; i < matCopy.rows; i++){
+        for(int j = 0; j < matCopy.cols; j++){
+            matCopy.at<uint8_t>(i, j) = originalMat.at<uint8_t>(j, i);
+        }
+    }
+    return matCopy;
 }
 
 void MainWindow::on_actionSelect_image_triggered()
@@ -54,16 +98,26 @@ void MainWindow::on_actionSelect_image_triggered()
         return;
     }
 
-    imageMat = imread(fileName.toStdString(), IMREAD_GRAYSCALE);
-    //Image* image = MatToImage(imageMat);
+    matOriginal = imread(fileName.toStdString(), IMREAD_GRAYSCALE);
+    //Image* image = MatToImage(matOriginal);
 
-    Mat imageInverse = GetInverseImage(imageMat);
-    Dialog* imageWindow = new Dialog(this, imageMat, "Imagen Original");
+    Mat matInverse = GetInverseImage(matOriginal);
+    Mat matMirror = GetMirrorImage(matOriginal);
+    Mat mat90DImage = GetMirror90DImage(matOriginal);
+    Mat matBinary = GetBinaryImage(matOriginal, 80);
+    Mat matMinValue = GetMinimumValueImage(matOriginal, matBinary);
+    Dialog* imageWindow = new Dialog(this, matOriginal, "Imagen Original");
     imageWindow->show();
-    Dialog* imageWindowInverse = new Dialog(this, imageInverse, "Imagen Inversa");
+    Dialog* imageWindowInverse = new Dialog(this, matInverse, "Imagen Inversa");
     imageWindowInverse->show();
-
-
+    Dialog* imageWindowMirror = new Dialog(this, matMirror, "Imagen Espejo");
+    imageWindowMirror->show();
+    //Dialog* imageWindowMirror90D = new Dialog(this, mat90DImage, "Imagen Espejo 90 Grados");
+    //imageWindowMirror90D->show();
+    Dialog* imageWindowBinary = new Dialog(this, matBinary, "Imagen Binaria");
+    imageWindowBinary->show();
+    Dialog* imageWindowMinValue = new Dialog(this, matMinValue, "Imagen minimo valor");
+    imageWindowMinValue->show();
 }
 
 
